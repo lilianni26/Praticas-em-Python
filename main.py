a7740332195924afd82164a11dc73d2baf4ce968
lilianni26 <lilibarbosanayara@gmail.com>
2024-10-04 18:39:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-Nome: Lilianni Nayara Barbosa Mendonça
-Matrícula: 202408745541
-Projeto 5: Qual é a sigla ?
+siglas = {
+    'API': 'Interface de Programação de Aplicativos',
+    'HTML': 'linguagem de Marcação de Hipertexto',
+    'CSS': 'Folhas de Estilo Cascata',
+    'JS': 'JavaScript',
+    'SQL': 'Linguagem de Consulta Estruturada',
+    'HTTP': 'Protocolo de Transferência de Hipertexto',
+    'FTP': 'Protocolo de Transferência de Arquivos',
+    'JSON': 'Notação de Objetos JavaScript',
+    'XML': 'Linguagem de MArcação Extensível',
+}
 
-Estado =
+print("Bem vindo ao aplicativo Siglas!")
+print("Digite 'sair' a qualquer momento para encerrar.")
+
+while True:
+    sigla = input("Digite uma sigla:XML").upper()
+
+    if XML == 'SAIR':
+        print("Saindo do aplicativo. Até logo!")
+        break
+
+        significado = siglas.get(TT, "Sigla não encontrada.")
+        print("TT"{siglas}:{significado}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
